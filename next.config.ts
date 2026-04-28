7b1933f2eaae5d6f41038399476124b91a66cbda
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2026-04-28 11:46:51 -0300

    config: add prensa jujuy to remotePatters for images

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         hostname: "joyueggtfcfzobphltme.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "https",
+        hostname: "media.prensa.jujuy.gob.ar",
+        pathname: "/**", // Esto permite cualquier imagen de ese dominio
+      },
     ],
   }
 };
